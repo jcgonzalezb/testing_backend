3ec8573eae9003baea88dfe4c2c9fbfa02e110cb
jcgonzalezb <jcgonzalez@gmail.com>
2022-03-13 21:23:11 -0500

    test_2

--- react_test_v2/client/src/App.js
+++ react_test_v2/client/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    /* fetch("/category/1").then( *//* Use this line for get id */
+    fetch("/category").then( /* Use this line for get all */
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+      {/* This block is for get by id */}
+      {/* {(typeof data.cat_id === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+              <label >Name of the appliance: {data.cat_nom}<br></br></label>
+              <label >description: {data.cat_desp}</label>
+        </div>
+      )} */}
+      {/* End block get by id */}
+
+      {/* This block is for get all */}
+      {(typeof data.results === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        data.results.map((value, i) => (
+          <div key={i}>
+            <label>Name of the appliance: {value["cat_id"]}<br></br></label>
+            <label>Name of the appliance: {value["cat_nom"]}<br></br></label>
+            <label >description: {value["cat_desp"]}</label><hr></hr>
+          </div>
+        ))
+        )}
+        {/* End block get all */}
+        
+        </div>
   );
 }
 
